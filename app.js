4e8a17d988f285c361ba980fcf480fb330d10b2c
jonrene <jrjonathanrene@gmail.com>
2022-08-17 22:50:48 -0400

    Pug View Engine
    changed application view engine to pug

--- app.js
+++ app.js
@@ -6,12 +6,21 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const book = require('./models/book');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
+
+// testing connection to database
+const sequelize = require("./models");
+try{
+  await sequelize.authenticate();
+}catch(error){
+  console.log(error);
+}
 
 app.use(logger('dev'));
 app.use(express.json());
